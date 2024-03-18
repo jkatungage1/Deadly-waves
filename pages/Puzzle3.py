3d0d90a7769696fd6a2e17e2d727fbd35c8a7e3f
jkatungage1 <jacques.katunga-mukendi@universite-paris-saclay.fr>
2024-03-19 00:01:23 +0100

    r

--- pages/Puzzle3.py
+++ pages/Puzzle3.py
@@ -5,8 +5,16 @@ from flask import request
 from Deadlywaves import EQUIPES,hots,T
 from Deadlywaves import PAGES
 
+img_width = 300
+img_height = 'auto'
+img_src= 'https://cdn.discordapp.com/attachments/1075182862598414377/1215771024243691712/image.png?ex=65fdf600&is=65eb8100&hm=67d065ae66a2a94f6cfddd2b59f6c93f3864b6b1f2da10176485a693026ca7f0&'
+pz31 = "https://cdn.discordapp.com/attachments/1075182862598414377/1219393658747617473/image.png?ex=660b23d7&is=65f8aed7&hm=43c6a75b77ece20007d6f5820aa43f95dde1537e70e87fa395986b71122436c0&"
+pz32 = "https://cdn.discordapp.com/attachments/1075182862598414377/1219393658290307133/image.png?ex=660b23d7&is=65f8aed7&hm=409672d2de03082343be474372dfa56b744c5335b66dbdc81da8bce2c776beea&"
+pz33 = "https://cdn.discordapp.com/attachments/1075182862598414377/1219393414953566380/image.png?ex=660b239d&is=65f8ae9d&hm=e7c6877850787f0c2b77d3a958a27f1ec7e2ea4d8edf5708f4cf2ff25042830e&"
+pz34 = "https://cdn.discordapp.com/attachments/1075182862598414377/1219393414588665886/image.png?ex=660b239d&is=65f8ae9d&hm=58a775e51f17b3d910c4e55fb241f04f3a2c3e855bfa5d993913e54ec6175e4e&"   
 
-img_src= 'https://cdn.discordapp.com/attachments/1075182862598414377/1215771024243691712/image.png?ex=65fdf600&is=65eb8100&hm=67d065ae66a2a94f6cfddd2b59f6c93f3864b6b1f2da10176485a693026ca7f0&'    
+
+
 d.register_page(__name__) #'/' means it's the maine page
 victoire = 'oui'
 
@@ -24,7 +32,7 @@ layout = html.Div([
                                 'margin-left' :'auto'
                                                   
             }),style={
-            'margin-top':'300px'
+            'margin-top':'200px'
         }),
             
     html.H1(id='Title3',
@@ -35,6 +43,40 @@ layout = html.Div([
                         'font-family':'monospace'
                     }),
     
+    
+    html.Img(id='pz31',
+                        src=pz31,style={
+                                            'width':str(img_width)+'px',
+                                            'height':str(img_height)+'px',
+                                            'padding-top': '0px',
+                                            'padding-left' :'30px'
+                                            
+    }),
+    
+    html.Img(id='pz32',
+                        src=pz32,style={
+                                            'width':str(img_width)+'px',
+                                            'height':str(img_height)+'px',
+                                            'padding-top': '0px',
+                                            'padding-left' :'30px'
+                                            
+    }),
+    html.Img(id='pz33',
+                        src=pz33,style={
+                                            'width':str(img_width)+'px',
+                                            'height':str(img_height)+'px',
+                                            'padding-top': '0px',
+                                            'padding-left' :'30px'
+                                            
+    }),
+    html.Img(id='pz34',
+                        src=pz34,style={
+                                            'width':str(img_width)+'px',
+                                            'height':str(img_height)+'px',
+                                            'padding-top': '0px',
+                                            'padding-left' :'30px'
+                                            
+    }),
     
     dcc.Input(id='input-on-submit', type='text',
             style={
@@ -65,7 +107,7 @@ def victory(n_clicks,value):
     
     if (n_clicks == 0 ) :
         print(n_clicks,'they are...')        
-        return 'They are coming !'
+        return 'Find the word to stop the bomb, the shockwaves would cause a disaster !'
         
     elif (n_clicks == 1 ) : 
         if(value == victoire):
@@ -88,7 +130,9 @@ def victory(n_clicks,value):
         
     # elif (n_clicks == 3 and value == victoire): 
     #     return 'vous êtes mort(s)!'
-    elif (n_clicks > 3 ) : return 'YOU DIED !'
+    elif (n_clicks > 3 and value != victoire ) : return 'YOU DIED !'
+    
+    else : return " Welp, that's gg's (sike) !"
     
     
     
